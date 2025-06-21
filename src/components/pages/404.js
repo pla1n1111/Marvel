23ab657875666e4fd4e1eb2161dbcf2a8fba3c27
Anton Slizhou <sonmil.100@gmail.com>
2025-06-21 19:06:54 +0300

    add seo with helmet

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,18 @@
+import { Helmet } from "react-helmet"
+
 import ErrorMessage from "../errorMessage/ErrorMessage"
 import { Link } from "react-router-dom"
 
 const Page404 = () => {
     return (
-        <div>
+        <div>   
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Error page"
+                    />
+                <title>Error</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 
--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
@@ -5,6 +7,13 @@ const ComicsPage = () => {
 
     return (
         <>
+            <Helmet>
+            <meta
+                name="description"
+                content="Page with list of out comics"
+                />
+            <title>Comics page</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
